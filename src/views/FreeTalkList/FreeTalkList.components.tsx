31f104a869ef8126bfdf9cc1c7535cf6f5a7fbbd
eliveli <60069112+eliveli@users.noreply.github.com>
2022-03-11 13:36:00 +0900

    Fix: 넘어오는 이미지 비율 유지를 위해 설정 변경 & 애니메이션 적용 등

--- src/views/FreeTalkList/FreeTalkList.components.tsx
+++ src/views/FreeTalkList/FreeTalkList.components.tsx
@@ -1,5 +1,6 @@
-// /* eslint-disable */
+/* eslint-disable */
 // 지금은 뷰 구성에 집중할 것임. 린트 무시하는 주석은 나중에 해제하기
+import { ThemeProvider } from "styled-components";
 import { FaRegComment, FaRegHeart } from "react-icons/fa";
 import {
   CreateDate,
@@ -17,48 +18,58 @@ import {
   IconNO,
   TalkPreview,
   BesideImgContainer,
+  TalkImgBox,
+  // setImgUrl,
 } from "./FreeTalkList.styles";
 
 type MyComponentProps = React.PropsWithChildren<{}>;
 
-export function FreeTalks({ children, ...props }: MyComponentProps) {
+export function FreeTalks({ children }: MyComponentProps) {
   return <TalkBG>{children}</TalkBG>;
 }
 
-FreeTalks.Talk = function () {
+FreeTalks.Talk = function (talkProps) {
+  // props or default props
+  const {
+    userImg = "https://cdn.pixabay.com/photo/2018/08/31/08/35/toys-3644073_960_720.png",
+    talkImg = "https://cdn.pixabay.com/photo/2018/08/31/08/35/toys-3644073_960_720.png",
+  } = talkProps;
+  const theme = {
+    userImg,
+    talkImg,
+  };
+  // setImgUrl(talkProps.img);
   return (
-    <Talk>
-      <UserImg
-        src="https://cdn.pixabay.com/photo/2018/08/31/08/35/toys-3644073_960_720.png"
-        alt="name"
-      />
-      <BesideImgContainer>
-        <FirstLineContainer>
-          <UserNameBox>
-            <UserName>Nana</UserName>
-            <CreateDate>22.01.01</CreateDate>
-          </UserNameBox>
-          <IconsBox>
-            <IconBox>
-              <FaRegHeart />
-              <IconNO>7</IconNO>
-            </IconBox>
-            <IconBox>
-              <FaRegComment />
-              <IconNO>5</IconNO>
-            </IconBox>
-          </IconsBox>
-        </FirstLineContainer>
+    <ThemeProvider theme={theme}>
+      <Talk>
+        <UserImg />
+        <BesideImgContainer>
+          <FirstLineContainer>
+            <UserNameBox>
+              <UserName>Nana</UserName>
+              <CreateDate>22.01.01</CreateDate>
+            </UserNameBox>
+            <IconsBox>
+              <IconBox>
+                <FaRegHeart />
+                <IconNO>7</IconNO>
+              </IconBox>
+              <IconBox>
+                <FaRegComment />
+                <IconNO>5</IconNO>
+              </IconBox>
+            </IconsBox>
+          </FirstLineContainer>
 
-        <TalkPreview>
-          <TalkTitle>꾸준히 인기 많은 해포 패러디 계의 탑 작품</TalkTitle>
-          <TalkImg
-            src="https://cdn.pixabay.com/photo/2018/08/31/08/35/toys-3644073_960_720.png"
-            alt="name"
-          />
-          <NovelTitle>[해리포터]지독한 후플푸프 - 곽정언</NovelTitle>
-        </TalkPreview>
-      </BesideImgContainer>
-    </Talk>
+          <TalkPreview>
+            <TalkTitle>꾸준히 인기 많은 해포 패러디 계의 탑 작품</TalkTitle>
+            <TalkImgBox>
+              <TalkImg />
+            </TalkImgBox>
+            <NovelTitle>[해리포터]지독한 후플푸프 - 곽정언</NovelTitle>
+          </TalkPreview>
+        </BesideImgContainer>
+      </Talk>
+    </ThemeProvider>
   );
 };
--- src/views/FreeTalkList/FreeTalkList.styles.tsx
+++ src/views/FreeTalkList/FreeTalkList.styles.tsx
@@ -1,4 +1,6 @@
-import styled from "styled-components";
+// /* eslint-disable */
+
+import styled, { keyframes } from "styled-components";
 
 export const TalkBG = styled.section`
   /* 모바일 */
@@ -18,6 +20,7 @@ export const TalkBG = styled.section`
 `;
 
 export const Talk = styled.article`
+  width: 100%;
   display: flex;
   padding: 12px 0;
   border-bottom: 1px solid rgba(100, 100, 100, 0.2);
@@ -35,12 +38,22 @@ export const FirstLineContainer = styled.div`
 
   border-bottom: 1px solid rgba(100, 100, 100, 0.1);
 `;
-export const BesideImgContainer = styled.div``;
+export const BesideImgContainer = styled.div`
+  width: 100%;
+`;
 
-export const UserImg = styled.img`
+// export function setImgUrl(img: string) {
+//   imgUrl = img;
+// }
+// let imgUrl = "https://cdn.pixabay.com/photo/2018/08/31/08/35/toys-3644073_960_720.png";
+export const UserImg = styled.div`
   border-radius: 50%;
-  width: 50px;
+  min-width: 50px;
   height: 50px;
+  background-image: url(${(props) => props.theme.userImg});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 `;
 export const UserNameBox = styled.div`
   display: flex;
@@ -55,13 +68,109 @@ export const CreateDate = styled.p`
   padding-left: 12px;
 `;
 export const TalkPreview = styled.div`
-  justify-content: space-between;
   padding-left: 12px;
   padding-top: 6px;
 `;
-export const TalkImg = styled.img`
-  border-radius: 10%;
+export const TalkImgBox = styled.div`
   width: 100%;
+
+  text-align: center; // 자식이미지 width 100% 아닐 때 가운데 정렬
+
+  position: relative;
+
+  overflow: hidden;
+
+  /* 모바일 */
+  height: 150px;
+  /* 태블릿 */
+  @media screen and (min-width: 768px) {
+    height: 200px;
+  }
+  /* PC - 모바일,태블릿과 뷰를 다르게 구성 */
+  @media screen and (min-width: 1024px) {
+    height: 250px;
+  }
+
+  /* overflow-x: hidden;  // y스크롤 넣을 경우 */
+  /* scroll-behavior: smooth; */
+`;
+const slideImgMobile = keyframes`
+  0% {
+    top: 0;
+  }
+  10% {
+    top: 0;
+  }
+  90%{
+    top: -110px;
+   }
+  100%{
+    top: -110px;
+   }
+  `;
+const slideImgTablet = keyframes`
+  0% {
+    top: 0;
+  }
+  10% {
+    top: 0;
+  }
+  90%{
+    top: -470px;
+   }
+  100%{
+    top: -470px;
+   }
+  `;
+const slideImgPC = keyframes`
+  0% {
+    top: 0;
+  }
+  10% {
+    top: 0;
+  }
+  90%{
+    top: -510px;
+   }
+  100%{
+    top: -510px;
+   }
+  `;
+export const TalkImg = styled.img`
+  position: absolute;
+  left: 0;
+
+  /* padding-top: 50%; */
+  width: 100%;
+  display: inline-block; // 자식이미지 width 100% 아닐 때 가운데 정렬
+
+  border-radius: 10%;
+
+  content: url(${(props) => props.theme.talkImg});
+
+  /* 모바일 */
+  animation-name: ${slideImgMobile};
+  animation-direction: alternate;
+  animation-iteration-count: infinite;
+  animation-timing-function: ease-in-out;
+  animation-duration: 2s; // 문자열로 넣으면 작동 안 함... (X)"1s"(X)
+  /* animation-delay: 1s; */
+  /* animation-fill-mode: forwards; //애니메이션 종료 후 마지막 keyframe 값 유지(중요!) */
+
+  /* 태블릿 */
+  @media screen and (min-width: 768px) {
+    animation-name: ${slideImgTablet};
+    animation-duration: 3.5s;
+  }
+  /* PC - 모바일,태블릿과 뷰를 다르게 구성 */
+  @media screen and (min-width: 1024px) {
+    animation-name: ${slideImgPC};
+    animation-duration: 4s;
+  }
+
+  &:hover {
+    animation-play-state: paused;
+  }
 `;
 export const IconsBox = styled.div`
   display: flex;
