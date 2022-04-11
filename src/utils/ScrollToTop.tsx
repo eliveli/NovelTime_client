61b0fd9226864793aebab3d2b09a327fc460aab8
eliveli <60069112+eliveli@users.noreply.github.com>
2022-04-11 23:45:22 +0900

    Fix : Scroll to top except when you click the back button

--- src/utils/ScrollToTop.tsx
+++ src/utils/ScrollToTop.tsx
@@ -1,11 +1,14 @@
 import { ReactChild, ReactChildren, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigationType } from "react-router-dom";
 
 function ScrollToTop({ children }: { children: ReactChild | ReactChildren }) {
   const location = useLocation();
-
+  const navigationType = useNavigationType();
   useEffect(() => {
-    window.scrollTo(0, 0);
+    // scroll to top except when you click the back button
+    if (navigationType !== "POP") {
+      window.scrollTo(0, 0);
+    }
   }, [location]);
 
   return <>{children}</>;
