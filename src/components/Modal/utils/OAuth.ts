7118d34afe738c4d9227c1b638d5fbd9c5b4b8e6
eliveli <60069112+eliveli@users.noreply.github.com>
2022-06-16 01:48:48 +0900

    Fix : Change domain name for google oauth
    " OOO .com" is required in domain name for google oauth

--- src/components/Modal/utils/OAuth.ts
+++ src/components/Modal/utils/OAuth.ts
@@ -6,6 +6,8 @@ const REST_API_KEY = process.env.REACT_APP_KAKAO_CLIENT_ID;
 // 개발 환경에 따라 달라짐. 환경변수 미리 설정 : process.env.REACT_APP_ENV
 // 터미널 CMD에서 set NODE_ENV=development
 const REDIRECT_URI =
-  process.env.REACT_APP_ENV === "production" ? "" : "http://domainfordev:3000/oauth/callback/kakao";
+  process.env.REACT_APP_ENV === "production"
+    ? ""
+    : "http://domainfordev.com:3000/oauth/callback/kakao";
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
