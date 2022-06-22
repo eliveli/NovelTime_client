9775addeac713e92ff17886ccdcdb1ad44637d67
eliveli <60069112+eliveli@users.noreply.github.com>
2022-06-23 01:56:09 +0900

    Refactor : Get user profile from google

--- src/components/Modal/utils/OAuth.ts
+++ src/components/Modal/utils/OAuth.ts
@@ -14,4 +14,4 @@ const REDIRECT_URI_GOOGLE =
 
 // oauth url
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY_KAKAO}&redirect_uri=${REDIRECT_URI_KAKAO}&response_type=code`;
-export const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${REST_API_KEY_GOOGLE}&response_type=token&redirect_uri=${REDIRECT_URI_GOOGLE}&scope=https://www.googleapis.com/auth/userinfo.email`;
+export const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${REST_API_KEY_GOOGLE}&response_type=token&redirect_uri=${REDIRECT_URI_GOOGLE}&scope=https://www.googleapis.com/auth/userinfo.profile`;
