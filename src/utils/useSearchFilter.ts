bdb5485ac2285a9579dffad556f48e4bbc7e9f20
eliveli <60069112+eliveli@users.noreply.github.com>
2023-02-28 14:27:36 +0900

    Fix : Get multiple search filters
    in next step  I'll get function setFilter to set multiple filters at once. I'm working on it

--- src/utils/useSearchFilter.ts
+++ src/utils/useSearchFilter.ts
@@ -9,6 +9,61 @@ import {
 } from "store/clientSlices/filterSlice";
 import { useAppDispatch, useAppSelector } from "store/hooks";
 
+// ** 아래 코드 제작 중 to treat multiple search filter at once for both pagination and infinite scroll
+export function useMultipleSearchFilter(
+  filterType1: "genre" | "searchType" | "searchWord" | "sortType" | "pageNo",
+  filterType2: "genre" | "searchType" | "searchWord" | "sortType" | "pageNo",
+  filterType3?: "genre" | "searchType" | "searchWord" | "sortType" | "pageNo",
+  filterType4?: "genre" | "searchType" | "searchWord" | "sortType" | "pageNo",
+  filterType5?: "genre" | "searchType" | "searchWord" | "sortType" | "pageNo",
+) {
+  const dispatch = useAppDispatch();
+
+  const genreFromState = useAppSelector((state) => state.filter.genre);
+  const searchTypeFromState = useAppSelector((state) => state.filter.searchType);
+  const searchWordFromState = useAppSelector((state) => state.filter.searchWord);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const filterTypesWithUndefined = [
+    filterType1,
+    filterType2,
+    filterType3,
+    filterType4,
+    filterType5,
+  ];
+
+  const filterTypes = filterTypesWithUndefined.filter((_) => _ !== undefined);
+
+  const currentFilters = { genre: "", searchType: "", searchWord: "" };
+
+  filterTypes.map((filterType) => {
+    if (filterType === "genre") {
+      const genreFromUrl = searchParams.get("genre");
+
+      const currentFilterValue = genreFromUrl ?? genreFromState;
+
+      currentFilters.genre = currentFilterValue;
+      //
+    } else if (filterType === "searchType") {
+      const searchTypeFromUrl = searchParams.get("searchType");
+
+      const currentFilterValue = searchTypeFromUrl ?? searchTypeFromState;
+
+      currentFilters.searchType = currentFilterValue;
+      //
+    } else if (filterType === "searchWord") {
+      const searchWordFromUrl = searchParams.get("searchWord");
+
+      const currentFilterValue = searchWordFromUrl ?? searchWordFromState;
+
+      currentFilters.searchWord = currentFilterValue;
+    }
+  });
+
+  return { currentFilters };
+}
+
 export default function useSearchFilter(
   filterType: "genre" | "searchType" | "searchWord" | "sortType" | "pageNo",
 ) {
