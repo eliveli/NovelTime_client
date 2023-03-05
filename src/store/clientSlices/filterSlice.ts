2d13ca0a6d6d908dede623ea2658d33a51a863eb
eliveli <60069112+eliveli@users.noreply.github.com>
2023-03-06 02:51:53 +0900

    Fix : Set filters with search list

--- src/store/clientSlices/filterSlice.ts
+++ src/store/clientSlices/filterSlice.ts
@@ -1,5 +1,6 @@
 /* eslint-disable no-param-reassign */
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { SortTypeFromFilter } from "./modalSlice";
 
 export type GenresFromFilter =
   | "All"
@@ -16,11 +17,31 @@ export type GenresFromFilter =
 
 export type SearchTypeFromFilter = "Title" | "Desc" | "Writer" | "Novel";
 
+type FilterType = "genre" | "searchType" | "searchWord" | "sortType" | "pageNo";
+type TalkFilters = {
+  genre: GenresFromFilter;
+  searchType: SearchTypeFromFilter;
+  searchWord: string;
+  sortType: SortTypeFromFilter;
+  pageNo: number;
+};
+
 export type IsFilterState = {
   genre: GenresFromFilter;
   searchType: SearchTypeFromFilter;
   searchWord: string;
   pageNo: number;
+
+  // to treat back page
+  talk: {
+    filters: TalkFilters;
+    list: any[];
+  };
+  // recommend: { filters: Filters; list: any[] };
+  // novel: { filters: Filters; list: any[] };
+  // sortOnly: { filters: Filters; list: any[] }; // let's talk and play in 소설 상세 페이지
+  // all: { filters: Filters; list: any[] }; // search for writings or novels in nav
+  //
 
   searchTextCtgr: string;
   searchContentCtgr: string;
@@ -33,11 +54,29 @@ const initialState: IsFilterState = {
   searchWord: "",
   pageNo: 1,
 
+  //
+  talk: {
+    filters: {
+      genre: "All",
+      searchType: "Title",
+      searchWord: "",
+      sortType: "작성일New",
+      pageNo: 1,
+    },
+    list: [],
+  },
+  // recommend: { filters: {}, list: [] },
+  // novel: { filters: {}, list: [] },
+  // sortOnly: { filters: {}, list: [] },
+  // all: { filters: {}, list: [] },
+  //
+
   searchTextCtgr: "",
   searchContentCtgr: "",
   searchNovel: { srchNovelId: "", srchNovelTitle: "title" },
 };
 
+// all filters for infinite scroll
 export const filterSlice = createSlice({
   name: "filter",
   initialState,
@@ -68,6 +107,23 @@ export const filterSlice = createSlice({
       state.pageNo = action.payload;
     },
 
+    setTalkList: (
+      state,
+      action: PayloadAction<{
+        filters?: TalkFilters;
+        list?: any[];
+      }>,
+    ) => {
+      const { filters, list } = action.payload;
+
+      if (filters) {
+        state.talk.filters = filters;
+      }
+      if (list) {
+        state.talk.list = list;
+      }
+    },
+
     setSearchTextCtgr: (state, action: PayloadAction<string>) => {
       state.searchTextCtgr = action.payload;
     },
@@ -95,6 +151,8 @@ export const {
   selectSearchType,
   setSearchWord,
   setPageNo,
+
+  setTalkList,
 
   setSearchTextCtgr,
   setSearchContentCtgr,
