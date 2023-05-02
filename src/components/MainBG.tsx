cb3e644a31c503f38a41099f7808112c1c31030a
eliveli <60069112+eliveli@users.noreply.github.com>
2023-05-02 13:07:29 +0900

    Comment : Leave comment that I couldn't fit the component to the bottom
    the component for writing comment in free-talk detail page for mobile and some tablet screen
    to the bottom of the screen

--- src/components/MainBG.tsx
+++ src/components/MainBG.tsx
@@ -16,9 +16,23 @@ export const MainBG = styled.main<{
   ${({ isWritingDetail }) =>
     isWritingDetail &&
     ` padding: 10px;
+
       @media screen and (min-width: 768px) {
         padding: 20px;
       }
+
+      // * this doesn't work
+      // *     @media screen and (max-width: 820px) {
+      // *       padding-bottom: 0px;
+      // *     }
+      // *  for a component for writing comment in free-talk detail page in mobile and some tablet screen
+      // *
+      // * I wanted to fit the component to the bottom of the screen, but I couldn't
+      // * it only worked when scroll-y bar existed
+      // * it couldn't work
+      // *   when scroll-y bar didn't exist
+      // *        and the height of canvas element that I never created but existed and was the first child of html element
+      // *                       was longer than html element
    `}
 
   /* 태블릿 */
