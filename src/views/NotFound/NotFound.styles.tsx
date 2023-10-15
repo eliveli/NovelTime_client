158164410d66a769cea7a64f1011c5ba6519eccd
eliveli <60069112+eliveli@users.noreply.github.com>
2023-10-16 00:06:16 +0900

    Design : Design the NotFound page

--- src/views/NotFound/NotFound.styles.tsx
+++ src/views/NotFound/NotFound.styles.tsx
@@ -1,1 +1,11 @@
-export {};
+import { styled } from "assets/styles/theme";
+
+export const NotFoundContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Img = styled.img``;
+
+export const ImgSource = styled.a``;
--- src/views/NotFound/index.tsx
+++ src/views/NotFound/index.tsx
@@ -1,5 +1,13 @@
-import React from "react";
+import MainBG from "components/MainBG";
+import { Img, ImgSource, NotFoundContainer } from "./NotFound.styles";
 
 export default function NotFound() {
-  return <div>Not Found</div>;
+  return (
+    <MainBG>
+      <NotFoundContainer>
+        <Img src="https://i.imgur.com/qa1fp4n.jpg" width="100%" alt="not found" />
+        <ImgSource href="http://www.freepik.com">Designed by stories / Freepik</ImgSource>
+      </NotFoundContainer>
+    </MainBG>
+  );
 }
