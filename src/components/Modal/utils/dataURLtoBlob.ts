ba45e026458267f66a380d1db41c9c6663a7dfcb
eliveli <60069112+eliveli@users.noreply.github.com>
2023-10-26 23:43:44 +0900

    Fix : Fix the lint errors and deprecated issue

--- src/components/Modal/utils/dataURLtoBlob.ts
+++ src/components/Modal/utils/dataURLtoBlob.ts
@@ -1,17 +1,17 @@
-export default function dataURLtoBlob(dataUrl: string): Blob | undefined {
+export default function dataURLtoBlob(dataUrl: string): Blob | null {
   const arr = dataUrl.split(","); // dataUrl is from canvas.toDataURL. it includes the base64-encoded image data but it is not base64
 
   const data = arr[0].match(/:(.*?);/);
-  if (data === null) return;
+  if (data === null) return null;
 
   const mime: string | null = data[1];
 
-  //   Buffer.from(data, 'base64')
-  const bstr = atob(arr[1]);
+  const bstr = window.atob(arr[1]);
   let n = bstr.length;
 
   const u8arr = new Uint8Array(n);
-  while (n--) {
+  while (n) {
+    n -= 1;
     u8arr[n] = bstr.charCodeAt(n);
   }
   return new Blob([u8arr], { type: mime });
